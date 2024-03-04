2ed9fde3f5b73fd58411accfec09a63875d6413a
mustafa <121257754+mustafaakalin@users.noreply.github.com>
2024-03-04 20:43:16 +0300

    .gitignore updated, exclude vendor, node_modules directory

---
+++ vendor/robsontenorio/mary/src/View/Components/Tab.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mary\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Component;
+
+class Tab extends Component
+{
+    public string $uuid;
+
+    public function __construct(
+        public ?string $name = null,
+        public ?string $label = null,
+        public ?string $icon = null
+    ) {
+        $this->uuid = "mary" . md5(serialize($this));
+    }
+
+    public function tabLabel(): string
+    {
+        return $this->icon
+            ? Blade::render("<x-mary-icon name='" . $this->icon . "' class='mr-2 whitespace-nowrap' label='" . $this->label . "' />")
+            : $this->label;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return <<<'HTML'
+                    <a
+                        class="hidden"
+                        :class="{ 'tab-active': selected === '{{ $name }}' }"
+                        data-name="{{ $name }}"
+                        x-init="
+                                tabs.push({ name: '{{ $name }}', label: {{ json_encode($tabLabel()) }} });
+                                Livewire.hook('morph.removed', ({el}) => {
+                                    if (el.getAttribute('data-name') == '{{ $name }}'){
+                                        tabs = tabs.filter(i => i.name !== '{{ $name }}')
+                                    }
+                                })
+                            "
+                    ></a>
+
+                    <div x-show="selected === '{{ $name }}'" role="tabpanel" {{ $attributes->class("tab-content py-5 px-1") }}>
+                        {{ $slot }}
+                    </div>
+                HTML;
+    }
+}
